838a357aab3840eda8a15c7b53fe2e4b8e6d40fc
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-01-22 21:49:12 +0300

    feat: add validation and error handling

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -8,23 +8,17 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(morgan('tiny'));
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+const errorHandling = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+  
+  next(error);
+}
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -36,13 +30,19 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (!body.content) {
@@ -56,43 +56,49 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then(savedNote => {
-    response.json(savedNote);
-  });
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote);
+    })
+    .catch(error => next(error));
 });
 
-app.put("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const checkNote = notes.find((note) => note.id === id);
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-  if (checkNote) {
-    const updatedNote = {
-      id: checkNote.id,
-      content: checkNote.content,
-      important: request.body.important,
-    };
-    
-    notes = notes.map((note) =>
-      note.id === updatedNote.id ? updatedNote : note,
-    );
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end();
+      }
 
-    return response.json(updatedNote);
-  }
+      note.content = content;
+      note.important = important;
 
-  return response.status(404).end();
+      return note.save().then(updatedNote => {
+        response.json(updatedNote);
+      })
+    })
+    .catch(error => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(error => next(error))
 });
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
+
+// handler of requests that result in errors
+app.use(errorHandling);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
