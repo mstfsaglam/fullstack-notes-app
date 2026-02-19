dd5dc444ac542195d739a267638db217d8aea408
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-02-20 00:02:10 +0300

    feat: add populate methot to show contents on users and notes

--- notes-backend/tests/note_api.test.js
+++ notes-backend/tests/note_api.test.js
@@ -161,7 +161,6 @@ describe('when there is initially some notes saved', () => {
         .expect('Content-Type', /application\/json/)
 
       const usersAtEnd = await helper.usersInDb()
-      console.log(result.body.error)
       assert(result.body.error.includes('expected `username` to be unique'))
 
       assert.strictEqual(usersAtEnd.length, usersAtStart.length)
