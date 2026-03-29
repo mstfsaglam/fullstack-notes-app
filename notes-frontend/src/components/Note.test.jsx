733ed5f6d53dd54857b2e44f48f45c22a8f7a553
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-03-30 01:29:31 +0300

    test: add test for important button

--- notes-frontend/src/components/Note.test.jsx
+++ notes-frontend/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -7,11 +8,30 @@ test('renders content', () => {
     important: true
   }
 
-  render(<Note note={note} />)
+  render(<Note note={note}/>)
 
   const element = screen.getByText('Component testing is done with react-testing-library')
 
   screen.debug(element)
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockHandler}/>
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  screen.debug(button)
   
-  // expect(element).toBeDefined()
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
