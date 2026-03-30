48c81507fcaa0993569f96daf7918fb6ec655ca2
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-03-30 20:18:18 +0300

    test: test for togglable component

--- notes-frontend/src/tests/Note.test.jsx
+++ notes-frontend/src/tests/Note.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Note from './Note'
+import Note from '../components/Note'
 
 test('renders content', () => {
   const note = {
