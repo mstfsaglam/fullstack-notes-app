c8dd9affec09922274995305a125fec1ed5e1227
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-04-01 23:12:27 +0300

    test:add placeholder to the content input and test it to find

--- notes-frontend/src/components/NoteForm.jsx
+++ notes-frontend/src/components/NoteForm.jsx
@@ -19,11 +19,15 @@ const NoteForm = ({ createNote }) => {
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
-        <input
-          type="text"
-          value={newNote}
-          onChange={event => setNewNote(event.target.value)}
-        />
+        <label>
+          content
+          <input
+            type="text"
+            value={newNote}
+            onChange={event => setNewNote(event.target.value)}
+            placeholder='write note content here'
+          />
+        </label>
         <button type="submit">save</button>
       </form>
     </div>
