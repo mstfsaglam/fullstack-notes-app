8fd178bec997f2a257436cd9198bf04241d63cbf
mstfsaglam <44318156+mstfsaglam@users.noreply.github.com>
2026-04-12 20:48:22 +0300

    test: simulate user login by writing on the forms

--- e2e-tests/tests/note_app.spec.js
+++ e2e-tests/tests/note_app.spec.js
@@ -1,11 +1,28 @@
 const { test, expect } = require('@playwright/test')
+const { describe } = require('node:test')
 
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:5173')
+describe('Note app', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173')
 
-  const locator = page.getByText('Notes')
-  await expect(locator).toBeVisible()
-  await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2026')).toBeVisible()
+    const locator = page.getByText('Notes')
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('Note app, Department of Computer Science,  University of Helsinki 2026')).toBeVisible()
+  })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await page.getByLabel('username').fill('mustafa')
+    await page.getByLabel('password').fill('Mustafa0*')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('mustafa logged in')).toBeVisible()
+  })
+
 })
 
 
